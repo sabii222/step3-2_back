172da2fd1a4ef9f0ab71d4960973d4939bd2218a
sabii222 <166728131+sabii222@users.noreply.github.com>
2024-11-27 18:05:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import os # 追加環境変数を取得するためのモジュール
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})  # CORS設定を更新
+CORS(app, resources={r"/api/*": {"origins": [
+    "tech0-gen-8-step3-testapp-node2-22.azurewebsites.net",
+    "http://localhost:3000" # ローカル開発用
+]
+                                }})  # CORS設定を更新
 
 @app.route('/', methods=['GET'])
 def hello():
